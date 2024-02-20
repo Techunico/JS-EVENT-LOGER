b252fc4abc4e78f7f2a99b059888bc4bc7c8d5e6
Praveen Kumar <praveen.42000@gmail.com>
2024-02-21 05:23:23 +0530

    additional packages added

--- core/config.js
+++ core/config.js
@@ -1,9 +1,23 @@
 export const config = {
     file: {
+        target: "file",
         filePath: "",
         fileNamePrefix: "" //By Default we will take Date as Prefix. Ex: 20240119eventlog.txt
     },
     firebase: {
-
+        target: "firebase",
+        config: {
+            apiKey: "AIzaSyCeUmPFqWNLcz41EDFzJZZAt_wxTtLPJfo",
+            authDomain: "js-tute.firebaseapp.com",
+            projectId: "js-tute",
+            storageBucket: "js-tute.appspot.com",
+            messagingSenderId: "557969571152",
+            appId: "1:557969571152:web:0af17b1e4d53856fbe4055",
+            measurementId: "G-TDT5QG9TM8"
+        }
+    },
+    mongodb: {
+        target: "mongodb",
+        connectionString: ""
     }
 }
--- core/createConfigFile.js
+++ core/createConfigFile.js
@@ -1,8 +1,22 @@
-// import { config } from './config'
+import { config } from './config.js'
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs'
 
 const args = process.argv.slice(2);
+let configs = config
+const rootDirectory = process.cwd();
 
-console.log(args)
 
-// let configs = config
+const filePath = `${rootDirectory}/event_logger_config.json`;
+const fileContent = JSON.stringify(configs, null, 2);
+
+// Write content to file
+fs.writeFile(filePath, fileContent, (err) => {
+    if (err) {
+        console.error('Error writing file:', err);
+    } else {
+        console.log('File created and content written successfully!');
+    }
+});
 
