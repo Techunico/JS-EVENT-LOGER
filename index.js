47be614e1c7d7e91a21cfc20058cc38d38d90d3a
Praveen Kumar <praveen.42000@gmail.com>
2024-02-27 08:12:29 +0530

    new changes

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
-import { compareNestedStructures } from './functions/compare'
+import { compareNestedStructures } from './functions/compare.js'
+import path from 'path';
+import fs from 'fs'
 
-export const recordEvent = async (data) => {
-    let oldData = data.oldData
-    let newData = data.newData
-    let action = data.action
-    let performedBy = data.performedBy
-    let module = data.module
+const recordEvent = async () => {
+    // let oldData = data.oldData
+    // let newData = data.newData
+    // let action = data.action
+    // let performedBy = data.performedBy
+    // let module = data.module
 
-    const diffrence = compareNestedStructures(oldData, newData)
+    // const diffrence = compareNestedStructures(oldData, newData)
+    // Call the function to load the configuration
+    const eventLoggerConfig = await loadEventLoggerConfig();
+
+    switch (eventLoggerConfig.target) {
+        case 'file':
+            break
+        default:
+            break
+    }
 
 
 }
+
+
+
+const loadEventLoggerConfig = async () => {
+    const filePath = path.join(process.cwd(), 'event_logger_config.json');
+    try {
+        const data = await fs.promises.readFile(filePath, 'utf-8');
+        const eventLoggerConfig = JSON.parse(data);
+        return eventLoggerConfig
+    } catch (error) {
+        console.error('Error loading JSON file:', error);
+    }
+}
+
+recordEvent()
